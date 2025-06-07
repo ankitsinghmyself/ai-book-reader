2c7e4f2d5138ae8f165c905556f2cacc14d2f745
Ankit Singh <anktisinghmyself@gmail.com>
2025-06-07 13:28:14 +0530

    new fix

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_credentials=True,
-    allow_origins=["*","https://ai-tutor-lilac.vercel.app/"],
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"]
 )
