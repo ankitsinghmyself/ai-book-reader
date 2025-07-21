c5e352a9e54e5b27baace3822e96c49c18c20a09
Ankit Singh <anktisinghmyself@gmail.com>
2025-07-21 07:28:26 +0530

    value updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,14 @@
 
 # 🔧 Pre-start commands
 echo "Running setup..."
-python preprocess_books.py
-python build_all_vector_indexes.py
+
 
 # 🚀 Start FastAPI app
 python -m venv .venv
 .\.venv\Scripts\activate  
+
+python preprocess_books.py
+python build_all_vector_indexes.py
+
 uvicorn main:app --reload
      
